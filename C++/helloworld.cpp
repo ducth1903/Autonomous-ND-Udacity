74a65ef09179d45b825ab4b2868af106d9f22db3
dtr <dtr@melexis.com>
2018-12-28 17:57:04 -0500

    C++

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -3,7 +3,12 @@ Hello World program
 - To compile: g++ helloworld.cpp -o helloworld.out
 - To run: ./helloworld.out
 */
+
+// Logcal Operators: &&, ||, !
+
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -18,5 +23,28 @@ int main() {
     MONTH myMonth = Mar;
     cout << "My month is " << myMonth << endl;
     
+    /*
+    FILE IO:
+    - Include <fstream>
+    - Create a stream (input, output, both)
+        - ofstream myfile; (writing to a file)      => i.e. ofstream myfile ("input.txt", ios::app);
+        - ifstream myfile; (for reading a file)     => i.e. ifstream myfile ("input.txt");  then while(getline(myfile, line)) {cout<< line <<endl;}
+        - fstream myfile; (for reading and writing a file)
+    - Open the file myfile.open("filename");
+    - Write or read the file
+    - Close the file with myfile.close();
+    */
+
+   /*
+   cin doesn't retrieve strings with space in between
+   => use getline(): getline(cin, userName);
+   */
+    string stringLength;
+    float inches = 0;
+    cout << "Enter number: ";
+    getline(cin, stringLength);
+    stringstream(stringLength) >> inches;   // convert input string to float
+    cout << "Entered: " << inches << endl;
+
     return 0;
 }
